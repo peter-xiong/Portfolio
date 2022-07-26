e36ff5379241ee0415a75aa6efd07678e80eb36f
Peter Xiong <peter.y.xiong@gmail.com>
2022-07-26 09:45:27 -0500

    Finished up draft of Portfolio Website

--- My-Site/app.js
+++ My-Site/app.js
@@ -1,7 +1,12 @@
 import { changeText, changeColors } from "./modules.js";
 
 const header = document.getElementById("myHeader");
-const callToAction = document.getElementsByClassName("cta");
+const callToAction = document.getElementById("cta");
 
-changeText(callToAction[0], "Call Me!");
-changeColors(header);
+header.addEventListener("click", () => {
+  changeColors(header);
+});
+
+callToAction.addEventListener("mouseover", () => {
+  changeText(callToAction, "Call Me!");
+});
--- My-Site/modules.js
+++ My-Site/modules.js
@@ -1,13 +1,13 @@
-function changeText(domElement, newText) {
-  domElement.innerHTML = newText;
-}
-
 function changeColors(domElement) {
   const r = Math.random() * 255;
   const g = Math.random() * 255;
   const b = Math.random() * 255;
 
   domElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function changeText(domElement, text) {
+  domElement.innerHTML = text;
 }
 
 export { changeText, changeColors };
